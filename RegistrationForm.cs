39ac2c78e766a1a9d910168582472a1c7b26888d
shree0211 <shreevyas0211@gmail.com>
2022-03-25 17:33:59 +0530

    Validating First Name

---
+++ RegistrationForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class RegistrationForm
+    {
+        private string firstName;
+        public void GetInfo()
+        {
+            firstName = GetValidName("First Name: ");
+        }
+
+        private string GetValidName(string message)
+        {
+            string name;
+            do
+            {
+                Console.Write(message);
+                name = Console.ReadLine();
+                if (IsValidName(name))
+                    return name;
+                else
+                    Console.WriteLine("Invalid! (First letter caps, Min 3 characters)");
+            } while (true);
+        }
+
+        private bool IsValidName(string name)
+        {
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+            return Regex.IsMatch(name, pattern);
+        }
+    }
+}
