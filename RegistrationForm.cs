9bfb51f63e24a5dd64aa7337379b41db6dbb3262
shree0211 <shreevyas0211@gmail.com>
2022-03-25 18:31:16 +0530

    Validating Contact number

--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
     internal class RegistrationForm
     {
-        private const string namePattern = @"^[A-Z][a-zA-Z]{2,}$";
-        private const string emailPattern = @"^[A-Za-z0-9]{3,}([.][A-Za-z0-9]{3,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$";
+        const string namePattern = @"^[A-Z][a-zA-Z]{2,}$";
+        const string emailPattern = @"^[A-Za-z0-9]{3,}([.][A-Za-z0-9]{3,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$";
+        const string mobilePattern = @"^[0-9]{2}[ ][0-9]{10}$";
 
         private string firstName;
         private string lastName;
         private string email;
+        private string mobile;
 
         public void GetInfo()
         {
             firstName = GetValidInfo("First Name: ", namePattern);
             lastName = GetValidInfo("Last Name: ", namePattern);
             email = GetValidInfo("Email: ", emailPattern);
+            mobile = GetValidInfo("Mobile: ", mobilePattern);
         }
 
         private static string GetValidInfo(string message, string pattern)
