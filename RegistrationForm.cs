73850ce1afa898bba778e5384586e64798c9559f
shree0211 <shreevyas0211@gmail.com>
2022-03-25 18:42:47 +0530

    Validating Password

--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -7,11 +7,14 @@ namespace UserRegistration
         const string namePattern = @"^[A-Z][a-zA-Z]{2,}$";
         const string emailPattern = @"^[A-Za-z0-9]{3,}([.][A-Za-z0-9]{3,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$";
         const string mobilePattern = @"^[0-9]{2}[ ][0-9]{10}$";
+        const string passwordPattern = @"^[\w\S]{8,}$";
 
         private string firstName;
         private string lastName;
         private string email;
         private string mobile;
+        private string password;
+
 
         public void GetInfo()
         {
@@ -19,6 +22,8 @@ namespace UserRegistration
             lastName = GetValidInfo("Last Name: ", namePattern);
             email = GetValidInfo("Email: ", emailPattern);
             mobile = GetValidInfo("Mobile: ", mobilePattern);
+            password = GetValidInfo("Password: ", passwordPattern);
+
         }
 
         private static string GetValidInfo(string message, string pattern)
